2d558023bd1779a77565e9afe26d2b466a138e97
invalidmarkup369 <73431900+invalidmarkup369@users.noreply.github.com>
2021-06-19 16:19:31 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+//jshint version:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({
+  extended :true
+}));
+
+app.get("/",function(req,res){
+  res.render("home");
+});
+app.get("/login",function(req,res){
+  res.render("login");
+})
+app.get("/register",function(req,res){
+  res.render("register");
+})
+
+app.listen("3000",function(){
+  console.log("Server is running at port 3000");
+});
