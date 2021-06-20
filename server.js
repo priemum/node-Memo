94abd201bfc6973c6115acbbc8f2944d2b362dd8
invalidmarkup369 <73431900+invalidmarkup369@users.noreply.github.com>
2021-06-20 20:12:20 +0200

    Heroku fix

--- server.js
+++ server.js
@@ -31,6 +31,5 @@ app.get("/memos",function(req,res){
   res.render("memos");
 })
 
-app.listen("3000",function(){
-  console.log("Server is running at port 3000");
-});
+app.listen(process.env.PORT || 3000);
+console.log("Server is running at port 3000");
