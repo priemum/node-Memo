afb84e2d13be001d1b9b01455f7f93f28d23511e
Gaber Elsayed <47481084+Gaber-sayed@users.noreply.github.com>
2021-07-16 01:25:43 +0200

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV !== "production"){
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"); 
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
